60abdcb6a334a73a370493028d2562c302e44ff3
Mauricio Juba <mauriciojuba@outlook.com>
2016-05-10 20:39:56 -0300

    Scripts de Transparência

---
+++ CheckForObstructedView.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckForObstructedView : MonoBehaviour {
+
+
+    private Camera myCamera;
+	public Transform PlayerHead,PlayerFoot;
+	public LayerMask ignorePlayer;
+	public Transform RayCam;
+
+    void Start()
+    {
+        myCamera = Camera.main;
+    }
+    void Update()
+    {
+        TestHeadObstruction();
+		TestBodyObstruction ();
+    }
+	void TestHeadObstruction()
+    {
+		Ray ray = new Ray (PlayerHead.position, myCamera.transform.position - PlayerHead.position);
+        RaycastHit[] hits;
+		hits = Physics.RaycastAll(ray,RayCam.position.y,ignorePlayer);
+        foreach (RaycastHit hit in hits)
+        {
+            Renderer R = hit.collider.GetComponent<Renderer>();
+            if (R == null) continue;
+			if (hit.collider.GetComponent<MakeTransparent>() == null)
+            {
+                hit.collider.gameObject.AddComponent<MakeTransparent>();
+            }
+            else
+            {
+				hit.collider.GetComponent<MakeTransparent>().makeTransparent();
+            }
+        }
+    }
+	void TestBodyObstruction()
+	{
+		Ray ray = new Ray (PlayerFoot.position, myCamera.transform.position - PlayerFoot.position);
+		RaycastHit[] hits;
+		hits = Physics.RaycastAll(ray,RayCam.position.y,ignorePlayer);
+		foreach (RaycastHit hit in hits)
+		{
+			Renderer R = hit.collider.GetComponent<Renderer>();
+			if (R == null) continue;
+			if (hit.collider.GetComponent<MakeTransparent>() == null)
+			{
+				hit.collider.gameObject.AddComponent<MakeTransparent>();
+			}
+			else
+			{
+				hit.collider.GetComponent<MakeTransparent>().makeTransparent();
+			}
+		}
+	}
+
+}
---
+++ MakeTransparent.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeTransparent : MonoBehaviour {
+
+	Material Standard, Transparent_Buildings, Transparent_Enemys;
+	float count = 10;
+	Color transColor;
+	Renderer render;
+
+	void Start () {
+		render = GetComponent<Renderer> ();
+		Transparent_Buildings = Resources.Load ("Materials/BuildingsTransparent") as Material;
+		Transparent_Enemys = Resources.Load ("Materials/EnemysTransparent") as Material;
+		Standard = render.sharedMaterial;
+		count = 10;
+		if(this.CompareTag("Building"))render.sharedMaterial = Transparent_Buildings;
+		else if(this.CompareTag("Enemy"))render.sharedMaterial = Transparent_Enemys;
+
+	}
+	void Update () {
+		transColor.a = Mathf.Abs(count)/10;
+		Transparent_Enemys.color = transColor;
+		Transparent_Buildings.color = transColor;
+	}
+	void LateUpdate(){
+		count += Time.deltaTime;
+		if (transColor.a >= 1) {
+			Destroy (this);
+		}
+	}
+	public void makeTransparent(){
+		count = 9;
+	}
+	void OnDestroy(){
+		render.sharedMaterial = Standard;
+	}
+}
