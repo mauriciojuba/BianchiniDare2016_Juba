62f06dcbded7c4571843df6cc5c29f1815fb2759
mauriciojuba <mauriciojuba@outlook.com>
2016-05-24 01:27:59 -0300

    Barreira

---
+++ Assets/Scripts/Utils/BarrierHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrierHandler : MonoBehaviour {
+
+    public Material Green;
+    public bool doorCollider;
+    Renderer rend;
+    bool openned;
+
+	void Start () {
+        rend = GetComponent<Renderer>();
+        doorCollider = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!doorCollider && !openned)
+        {
+            Invoke("OpenDoor", 1f);
+        }
+	}
+    public void OpenDoor()
+    {
+        rend.sharedMaterial = Green;
+        this.GetComponent<BoxCollider>().enabled = false;
+        openned = true;
+    }
+}
---
+++ Assets/Scripts/Utils/Computer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Computer : MonoBehaviour {
+
+    public Material Green;
+    public BarrierHandler barreira;
+    bool canSwitch, openned;
+    Renderer rend;
+
+    void Start () {
+        rend = GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (canSwitch && !openned) openDoor();
+        Debug.Log(canSwitch);
+	}
+    void openDoor()
+    {
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            rend.sharedMaterial = Green;
+            barreira.doorCollider = false;
+            openned = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider hit)
+    {
+        if (hit.gameObject.CompareTag("Player"))
+        {
+            canSwitch = true;
+        }
+    }
+    void OnTriggerExit(Collider hit)
+    {
+        if (hit.gameObject.CompareTag("Player"))
+        {
+            canSwitch = false;
+        }
+    }
+}
