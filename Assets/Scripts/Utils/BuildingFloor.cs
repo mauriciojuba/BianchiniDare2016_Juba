fce7cdc6b0114938b6608fdd1e67be7f17b37640
mauriciojuba <mauriciojuba@outlook.com>
2016-05-29 22:04:08 -0300

    FloorIndividualTransparency

---
+++ Assets/Scripts/Utils/BuildingFloor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingFloor : MonoBehaviour {
+
+    public int floor;
+	void OnTriggerEnter(Collider hit)
+    {
+        if (hit.CompareTag("Player"))
+        {
+            BuildingsHandler.Instance.changeFloor(floor);
+        }
+    }
+}
--- Assets/Scripts/Utils/BuildingsHandler.cs
+++ Assets/Scripts/Utils/BuildingsHandler.cs
@@ -3,13 +3,89 @@ using System.Collections;
 
 public class BuildingsHandler : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public static BuildingsHandler Instance;
+    public GameObject Floor1, Floor2, Floor3, Top;
+
+    void Start()
+    {
+        Instance = this;
+    }
+    public void changeFloor(int _floor)
+    {
+        if (_floor == 0) Outside();
+        if (_floor == 1) Floor1On();
+        if (_floor == 2) Floor2On();
+        if (_floor == 3) Floor3On();
+    }
+    void Outside()
+    {
+        Floor1.SetActive(true);
+        Floor2.SetActive(true);
+        Floor3.SetActive(true);
+        Top.SetActive(true);
+    }
+    void Floor1On()
+    {
+        Floor1.SetActive(true);
+        Floor2.SetActive(false);
+        Floor3.SetActive(false);
+        Top.SetActive(false);
+    }
+    void Floor2On()
+    {
+        Floor2.SetActive(true);
+        Floor3.SetActive(false);
+        Top.SetActive(false);
+    }
+    void Floor3On()
+    {
+        Floor3.SetActive(true);
+        Top.SetActive(false);
+    }
+
+
+    //perguntar pro professor
+    //Renderer[] Floor1r, Floor2r, Floor3r, Topr;
+
+    //void Start()
+    //{
+    //    Instance = this;
+    //    getMeshRenderer();
+    //}
+
+    //public void changeFloor(int _floor)
+    //{
+    //    if (_floor == 0) Outside();
+    //    if (_floor == 1) Floor1On();
+    //    if (_floor == 2) Floor2On();
+    //    if (_floor == 3) Floor3On();
+    //}
+    //void getMeshRenderer()
+    //{
+    //    Floor1r = Floor1.GetComponentsInChildren<Renderer>();
+    //    Floor2r = Floor2.GetComponentsInChildren<Renderer>();
+    //    Floor3r = Floor3.GetComponentsInChildren<Renderer>();
+    //}
+    //void Floor1On()
+    //{
+    //    for(int i = 0; i <= Floor1r.Length; i++) if (!Floor1r[i].enabled) Floor1r[i].enabled = true;
+    //    for (int j = 0; j <= Floor2r.Length; j++) if (Floor2r[j].enabled) Floor2r[j].enabled = false;
+    //    for (int k = 0; k <= Floor3r.Length; k++) if (Floor3r[k].enabled) Floor3r[k].enabled = false;
+    //    for (int l = 0; l <= Topr.Length; l++) if (Topr[l].enabled) Topr[l].enabled = false;
+    //}
+    //void Floor2On()
+    //{
+    //    for (int j = 0; j <= Floor2r.Length; j++) if (!Floor2r[j].enabled) Floor2r[j].enabled = true;
+    //    for (int k = 0; k <= Floor3r.Length; k++) if (Floor3r[k].enabled) Floor3r[k].enabled = false;
+    //    for (int l = 0; l <= Topr.Length; l++) if (Topr[l].enabled) Topr[l].enabled = false;
+    //}
+    //void Floor3On()
+    //{
+    //    for (int k = 0; k <= Floor3r.Length; k++) if (!Floor3r[k].enabled) Floor3r[k].enabled = true;
+    //    for (int l = 0; l <= Topr.Length; l++) if (Topr[l].enabled) Topr[l].enabled = false;
+    //}
+    //void Outside()
+    //{
+    //    for (int l = 0; l <= Topr.Length; l++) if (!Topr[l].enabled) Topr[l].enabled = true;
+    //}
 }
